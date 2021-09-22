b3e9afd84a08cfa3738cf7ec7f32081415ddb967
RijalArul <fakhrulmrijal7599@gmail.com>
2021-09-23 04:52:35 +0700

    Added Navbar & card template

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,19 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <nav class="navbar navbar-light" style="background-color: #978cdc">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" style="color: white">
+          <img
+            src="../assets/discord-logo-png-7617.png"
+            alt=""
+            width="110"
+            height="70"
+            class="d-inline-block align-text-center"
+            style=""
+          />
+        Discord Alert App
+        </a>
+      </div>
+    </nav>
+  </div>
 </template>
